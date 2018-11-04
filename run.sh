422c3f6a5bc6ff19769f5ca3e3bc3cd55518a3be
yulahuyed <yulahuyed@vpsorg.top>
2018-11-04 15:54:46 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+envsubst < passwd_template > /tmp/passwd
+export LD_PRELOAD=/usr/lib/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+export PATH=$HOME/aria2:$PATH
+export RCLONE_CONFIG=$HOME/config/rclone.conf
+
+
+cat << EOF >> ~/.profile
+export LD_PRELOAD=/usr/lib/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+export PATH=$HOME/aria2:$PATH
+export RCLONE_CONFIG=$HOME/config/rclone.conf
+EOF
+
+cat << EOF >> ~/.bashrc
+export LD_PRELOAD=/usr/lib/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+export PATH=$HOME/aria2:$PATH
+export RCLONE_CONFIG=$HOME/config/rclone.conf
+EOF
+
+
 if [ "$extend_script" ] && [ ! -f "$HOME/config/extend.sh" ]
 then
 curl -L -o $HOME/config/extend.sh "$extend_script"
@@ -15,7 +42,7 @@ fi
 mkdir /tmp/Downloads
 touch aria2.session
 touch /tmp/dht.dat
-nohup ./gotty -p 7900 -w bash >/dev/null 2>&1 &
+nohup ./gotty -p 7900 -w bash -c "${GOTTY_USER}:${GOTTY_PASS}" >/dev/null 2>&1 &
 
 nohup ./aria2c --conf-path=aria2.conf >/dev/null 2>&1 &
 
